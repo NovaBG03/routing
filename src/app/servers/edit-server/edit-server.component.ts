40c33c65ad6e715619766696f429ae4d65289c95
Nikita Koynov <nikicha03@gmail.com>
2020-08-20 19:12:06 +0300

    edit server button

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { ServersService } from '../servers.service';
+import {ServersService} from '../servers.service';
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-edit-server',
@@ -8,16 +9,31 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
   serverName = '';
   serverStatus = '';
+  isEditAllowed= false;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+
+    this.route.params
+      .subscribe((params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+        this.serverName = this.server.name;
+        this.serverStatus = this.server.status;
+      })
+
+    this.isEditAllowed = this.route.snapshot.queryParams['allowEdit'] === '1';
+    this.route.queryParams
+      .subscribe((routeParams: Params) => this.isEditAllowed = routeParams['allowEdit'] === '1');
   }
 
   onUpdateServer() {
